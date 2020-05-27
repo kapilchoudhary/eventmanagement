1a3b87de3cf13437060d8e90e34c4a62ed242f6e
Kapil <kchoudhary006@gmail.com>
2020-05-27 20:06:54 +0530

    Add bootstrap designing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 	    delete 'unattend'
 	  end
   end
-  # get '/join_event', to: 'events#join_event', as: :join_event
   devise_for :users
   root 'home#index'
 end
